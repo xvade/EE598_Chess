b085f2b75a4f01559b02dced1dac55408b358076
xvade <simon@vantuyl.com>
2026-02-12 20:04:38 -0800

    finished HW_X_1

--- HW_X_1.lean
+++ HW_X_1.lean
@@ -5,5 +5,7 @@ https://chatgpt.com/share/698bb62f-f8f4-8013-878e-1b9cc03e2285
 https://chatgpt.com/share/698bb61c-5fa0-8013-860f-3e738e899ed6
 https://chatgpt.com/share/698bb60a-8b44-8013-8bad-8f336c242e2d
 https://chatgpt.com/share/698bb5f6-55fc-8013-9c9c-dd7fd8e31f62
+
+I'm currently planning to contribute to CSlib, probably by adding simp flags to relevant lemmas.
 -/
 end HW_X_1
